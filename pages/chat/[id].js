5ce7c5a7d6ce0d99be08468e9dd3d29444c206eb
omekagu joseph <omekaguj@gmail.com>
2022-08-29 16:50:54 +0100

    my commit

--- pages/chat/[id].js
+++ pages/chat/[id].js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import Head from 'next/head';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import ChatScreen from '../../Components/ChatScreen';
